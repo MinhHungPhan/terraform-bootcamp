11228122f208d768d8e55382ed75ff6726703ef4
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-06-29 05:26:47 -0700

    added main.tf to configure Docker provider and Node-RED image resource

---
+++ 03-terraform-docker/02-basics-of-terraform/03-your-first-terraform-apply/terraform-docker/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 2.15.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red:latest"
+}
