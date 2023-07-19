9088c504168203f51c19c843c990d4665a680b82
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-07-19 13:48:33 +0200

    added main.tf for Multiple Resources and Count

---
+++ 03-terraform-docker/04-resources-and-expressions/03-multiple-resources-and-count/terraform-docker/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.15.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red:latest"
+}
+
+resource "random_string" "random" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
+resource "docker_container" "nodered_container" {
+  count = 2
+  name  = join("-", ["nodered", random_string.random[count.index].result])
+  image = docker_image.nodered_image.latest
+
+  ports {
+    internal = 1880
+    # external = 1880
+  }
+}
+
+output "IP-Address" {
+  value       = join(":", [docker_container.nodered_container[0].ip_address, docker_container.nodered_container[0].ports[0].external])
+  description = "The IP address and external port of the container"
+}
+
+output "Container-name" {
+  value       = docker_container.nodered_container[0].name
+  description = "The name of the container"
+}
+
+output "IP-Address2" {
+  value       = join(":", [docker_container.nodered_container[1].ip_address, docker_container.nodered_container[1].ports[0].external])
+  description = "The IP address and external port of the container"
+}
+
+output "Container-name2" {
+  value       = docker_container.nodered_container[1].name
+  description = "The name of the container"
+}
