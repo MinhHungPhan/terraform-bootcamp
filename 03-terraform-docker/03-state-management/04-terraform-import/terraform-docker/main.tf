a676ca3e82193671f0231060968f3b1bccd94f7f
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-08-15 10:01:46 +0200

    added main.tf for Terraform Import

---
+++ 03-terraform-docker/03-state-management/04-terraform-import/terraform-docker/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.15.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red:latest"
+}
+
+resource "random_string" "random" {
+  count   = 1
+  length  = 4
+  special = false
+  upper   = false
+}
+
+resource "docker_container" "nodered_container" {
+  count = 1
+  name  = join("-", ["nodered", random_string.random[count.index].result])
+  image = docker_image.nodered_image.latest
+
+  ports {
+    internal = 1880
+    # external = 1880
+  }
+}
+
+resource "docker_container" "nodered_container2" {
+  name  = "[Enter the container name that is not tracked by the state file]"
+  image = docker_image.nodered_image.latest
+}
+
+output "Container-name" {
+  value       = docker_container.nodered_container[*].name
+  description = "The name of the container"
+}
+
+output "IP-Address" {
+  value       = [for i in docker_container.nodered_container[*]: join(":", [i.ip_address], i.ports[*]["external"])]
+  description = "The IP address and external port of the container"
+}
