578b79fd9d0eeec032acfd827e2d0af66ae8f66e
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-11-18 14:53:12 +0100

    added main.tf for Terraform Sensitive Variables

---
+++ 03-terraform-docker/05-variables-and-outputs/06-hiding-sensitive-variables-from-cli/terraform-docker/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.15.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Resources
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red:latest"
+}
+
+resource "random_string" "random" {
+  count   = var.container_count
+  length  = 4
+  special = false
+  upper   = false
+}
+
+resource "docker_container" "nodered_container" {
+  count = var.container_count
+  name  = join("-", ["nodered", random_string.random[count.index].result])
+  image = docker_image.nodered_image.latest
+
+  ports {
+    internal = var.int_port
+    external = var.ext_port
+  }
+}
