a5c3253d5d63ba3a60afe959ec2968c4b91640a5
Minh Hung Phan <minhhung.phan2701@gmail.com>
2024-02-26 23:01:04 +0100

    updated main.tf to utilize terraform.workspace

--- 03-terraform-docker/06-advanced-terraform-concepts/08-referencing-your-workspaces/terraform-docker/main.tf
+++ 03-terraform-docker/06-advanced-terraform-concepts/08-referencing-your-workspaces/terraform-docker/main.tf
@@ -18,7 +18,7 @@ resource "null_resource" "dockervol" {
 }
 
 resource "docker_image" "nodered_image" {
-  name = lookup(var.image, var.env)
+  name = lookup(var.image, terraform.workspace)
 }
 
 resource "random_string" "random" {
@@ -30,12 +30,12 @@ resource "random_string" "random" {
 
 resource "docker_container" "nodered_container" {
   count = local.container_count
-  name  = join("-", ["nodered", random_string.random[count.index].result])
+  name  = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
 
   ports {
     internal = var.int_port
-    external = lookup(var.ext_port, var.env)[count.index]
+    external = lookup(var.ext_port, terraform.workspace)[count.index]
   }
   volumes {
     container_path = "/data"
