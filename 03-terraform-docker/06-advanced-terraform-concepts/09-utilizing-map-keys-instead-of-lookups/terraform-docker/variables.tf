8675a183f9739fcbc60a89b4ee9aa5687c60ab96
Minh Hung Phan <minhhung.phan2701@gmail.com>
2024-03-20 09:53:16 +0100

    added terraform-docker for Utilizing Map Keys instead of Lookups

---
+++ 03-terraform-docker/06-advanced-terraform-concepts/09-utilizing-map-keys-instead-of-lookups/terraform-docker/variables.tf
@@ -0,0 +1,37 @@
+# Variables
+
+variable "int_port" {
+  type        = number
+  default     = 1880
+  validation {
+    condition     = var.int_port == 1880
+    error_message = "The internal port must be 1880."
+  }
+}
+
+variable "image" {
+  type = map
+  description = "Image for container"
+  default = {
+      dev = "nodered/node-red:latest"
+      prod = "nodered/node-red:latest-minimal"
+  }
+}
+
+variable "ext_port" {
+  type = map
+
+  validation {
+    condition     = max(var.ext_port["dev"]...) <= 65535 && min(var.ext_port["dev"]...) >= 1980
+    error_message = "The external port for dev must be in the range 1980 - 65535."
+  }
+
+  validation {
+    condition     = max(var.ext_port["prod"]...) < 1980 && min(var.ext_port["prod"]...) >= 1880
+    error_message = "The external port for prod must be in the range 1880 - 1979."
+  }
+}
+
+locals {
+  container_count = length(lookup(var.ext_port, terraform.workspace))
+}
