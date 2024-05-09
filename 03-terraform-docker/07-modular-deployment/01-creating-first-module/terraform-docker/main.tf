bb69398d968af76c1832cd5be21f9f8ecf668c02
Minh Hung Phan <minhhung.phan2701@gmail.com>
2024-05-09 14:20:45 +0200

    removed providers from main.tf

--- 03-terraform-docker/07-modular-deployment/01-creating-first-module/terraform-docker/main.tf
+++ 03-terraform-docker/07-modular-deployment/01-creating-first-module/terraform-docker/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.15.0"
-    }
-  }
-}
-
-provider "docker" {}
-
 # Resources
 
 resource "null_resource" "dockervol" {
